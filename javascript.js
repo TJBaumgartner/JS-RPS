8354320cd9afcafaa2e1d0ad1bfbc4f56ad6aac2
TJBaumgartner <tnorth6@gmail.com>
2023-04-02 10:28:39 -0700

    added comments for clarity

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//declare variables
 let playerScore = 0;
 let computerScore = 0;
 let playerSelection;
@@ -6,7 +7,10 @@ let pScore = document.getElementById("pScore");
 let cScore = document.getElementById("cScore");
 let dialogueContent = document.getElementById("dialogueContent");
 let winner = '';
-
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+const buttons = Array.from(document.querySelectorAll('button'));
 
 //Randomize computer Choice
 function getComputerChoice(){
@@ -23,11 +27,7 @@ function getComputerChoice(){
     }
     return computerChoice;
 }
-const button1 = document.getElementById('button1');
-const button2 = document.getElementById('button2');
-const button3 = document.getElementById('button3');
 
-const buttons = Array.from(document.querySelectorAll('button'));
 //Get player Selection
 button1.addEventListener('click', () =>{
     playerSelection = 'Rock';
@@ -38,6 +38,7 @@ button2.addEventListener('click', () =>{
 button3.addEventListener('click', () =>{
     playerSelection = 'Scissors';
 });
+
 //When a button is clicked run the playRound function until someone gets 5 points
 buttons.forEach(button => button.addEventListener('click', () =>{
     if(playerScore < 5 && computerScore < 5){
@@ -47,21 +48,12 @@ buttons.forEach(button => button.addEventListener('click', () =>{
     pScore.textContent = playerScore;
     cScore.textContent = computerScore;
 
-
-    //Dialogue for player wins
- 
-
-
-
-
-
     if(playerScore ===5){
         dialogueContent.textContent = 'Game Over! You beat the machine!';
     }
     if(computerScore ===5){
         dialogueContent.textContent = 'Game Over! The machine beat you!';
     }
-
 }));
 
 //Function to play the round
@@ -120,70 +112,3 @@ function updateResult(winner){
     
     }
 }
-
-
-//NEXT TIME YOU COME BACK TERRENCE MAKE THE UI LOOK GOOD, GAME WORKS!
-
-
-/**function game(){
-    let scorePlayer = 0;
-    let scoreComputer = 0;
-    let gamesTied = 0;
-    let winner = false;
-    while(winner == false){  
-        let computerSelection = getComputerChoice();
-        playerSelection = prompt('What do you choose?');
-        playRound(playerSelection, computerSelection);
-        let outcome = playRound(playerSelection, computerSelection);
-        let gameResult = 0;
-        if(outcome == 'Draw! Rock ties Rock'){
-            gameResult = 3;
-        } 
-        if(outcome == 'Draw! Paper ties Paper'){
-            gameResult = 3;
-        }  
-        if(outcome == 'Draw! Scissors ties Scissors'){
-            gameResult = 3;
-        }   
-        if(outcome == 'You Lose! Scissors beats Paper'){
-            gameResult = 2;
-        }   
-        if(outcome == 'You Lose! Rock beats Scissors'){
-            gameResult = 2;
-        }   
-        if(outcome == 'You Lose! Paper beats Rock'){
-            gameResult = 2;
-        }   
-        if(outcome == 'You win! Scissors beats Paper'){
-            gameResult = 1;
-        }   
-        if(outcome == 'You win! Rock beats Scissors'){
-            gameResult = 1;
-        }     
-        if(outcome == 'You Win! Paper beats Rock'){
-            gameResult = 1;
-        }    
-        if(gameResult == 2){
-            scoreComputer++
-        }
-        if(gameResult == 3){
-            gamesTied++;
-        }
-        if(gameResult == 1){
-            scorePlayer++;
-        }
-        if(scorePlayer == 5){
-            console.log('You Win!')
-            winner = true;
-            break;
-        }
-        if(scoreComputer == 5){
-            console.log('You Lose!')
-            winner = true;
-            break;
-        }
-        let gameScore = ('Your score is '+ scorePlayer + ' wins, ' + gamesTied + ' ties, and ' + scoreComputer + ' losses');
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(gameScore);
-    }
-}**/
